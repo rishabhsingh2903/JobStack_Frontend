ac0566fc2b5bd5de62d3800fb0ee0dacccaeaaed
rishabh <rishabhvns2000@gmail.com>
2024-06-19 20:28:36 -0700

    added deploy.yml

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,8 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter
+    basename={import.meta.env.DEV ? '/' : '/JobStack_Frontend/'}>
     <App />
     </BrowserRouter>
 
